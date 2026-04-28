cac7849cb8b894a0b9cebf851cc090d6eb21c0a2
IBEMBEJOASH <ibembejoash9@gmail.com>
2026-04-28 11:00:46 +0300

    Updated StudentIdCard with elevatedcard

--- app/src/main/java/ug/ac/ndejje/ndejjewelcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/ndejjewelcome/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -110,6 +113,19 @@ fun StudentInfo() {
         }
 
     }
+}
+
+@Composable
+fun StudentIdCard() {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f)
+        )
+    ) { }
 }
 
 @Preview(showBackground = true)
