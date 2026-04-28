5370b6e72e81e8ef7df20c72d0750548224fc921
IBEMBEJOASH <ibembejoash9@gmail.com>
2026-04-28 09:15:46 +0300

    Wrapped with a column

--- app/src/main/java/ug/ac/ndejje/ndejjewelcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/ndejjewelcome/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -36,28 +38,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StudentInfo() {
-    Text(
-        text = stringResource(R.string.Student_name),
-        style = MaterialTheme.typography.headlineSmall,
-        fontWeight = FontWeight.Bold,
-        color = MaterialTheme.colorScheme.primary
-    )
-    Text(
-        text = stringResource(R.string.Programme).uppercase(),
-        style = MaterialTheme.typography.labelMedium,
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp)
-    )
-    Text(
-        text = "REG NO.:",
-        style = MaterialTheme.typography.labelLarge,
-        fontWeight = FontWeight.ExtraBold
-    )
-    Text(
-        text = "24/2/314/D/335",
-        style = MaterialTheme.typography.bodyLarge,
-        fontFamily = FontFamily.Monospace
-    )
+    Column(
+        modifier = Modifier.padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.Student_name),
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = stringResource(R.string.Programme).uppercase(),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp)
+        )
+        Text(
+            text = "REG NO.:",
+            style = MaterialTheme.typography.labelLarge,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Text(
+            text = "24/2/314/D/335",
+            style = MaterialTheme.typography.bodyLarge,
+            fontFamily = FontFamily.Monospace
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
